973ecbe527ef6e5daf70509f65caa8e44259059e
Andrew Woods <atwoods1@gmail.com>
2017-11-16 14:31:38 -0800

    Move bash aliases to separate file
    Makes it easier to re-use compoenents.

--- opal.bash
+++ opal.bash
@@ -10,5 +10,6 @@ source $OPAL_DIR/bash/util.bash
 source $OPAL_DIR/bash/functions.developer.bash
 source $OPAL_DIR/bash/functions.files-and-dirs.bash
 source $OPAL_DIR/bash/bashrc
+source $OPAL_DIR/bash/aliases
 
 
