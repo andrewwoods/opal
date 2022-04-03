bbcba8d8eb7b165ecc2d83e0aeebba956ca996db
Andrew Woods <services@awoods.net>
2022-04-03 16:05:11 -0400

    update git_parse_branch output
    Remove the parentheses from around the branch name. This makes its
    easier to reuse.

--- bash/functions.developer.bash
+++ bash/functions.developer.bash
@@ -64,7 +64,7 @@ function htstatus()
 #
 function parse_git_branch()
 {
-	git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
+	git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/'
 }
 
 
