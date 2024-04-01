c7d7eeaf068a66ada391321e0f0df2baf5b7edc7
Andrew Woods <services@awoods.net>
2024-03-31 22:07:33 -0400

    Move strlen to developer functions
    Renamed it to opal:str_length to better comply with Opal convention.

--- bash/bashrc.bash
+++ bash/bashrc.bash
@@ -267,11 +267,3 @@ function matrix() {
     done | awk '{ letters=$5; c=$4; letter=substr(letters,c,1);a[$3]=0;for (x in a) {o=a[x];a[x]=a[x]+1; printf "\033[%s;%sH\033[2;32m%s",o,x,letter; printf "\033[%s;%sH\033[1;37m%s\033[0;0H",a[x],x,letter;if (a[x] >= $1) { a[x]=0; } }}'
 }
 
-#
-# strlen - get the length of a string
-#
-# @param string
-#
-function strlen {
-    echo ${#1}
-}
