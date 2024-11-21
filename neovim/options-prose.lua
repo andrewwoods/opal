ea88aee8200a1a31f1b173a098e32a35e2cb5431
Andrew Woods <services@awoods.net>
2024-11-21 18:25:49 -0500

    Add options-prose.lua to neovim folder
    This is a basic options setup for writing text documents. This provides
    you a usable Neovim environment quickly. Copying this file to the
    project folder where you write your text documents, will modify the
    settings in your global options - to use settings for writers.

---
+++ neovim/options-prose.lua
@@ -0,0 +1,20 @@
+--[[
+
+Prose Options File
+
+These options are for writing prose (documents) that should override the
+defaults used for writing code.
+
+NOTE: Require this file for getting started quickly. You may want to copy this
+to your dotfiles, so you can make local modifications.
+
+--]]
+
+vim.opt.wrap = false
+vim.opt.textwidth = 72
+
+vim.opt.scrolloff = 8
+vim.opt.sidescroll = 8
+vim.opt.sidescrolloff = 8
+
+vim.opt.colorcolumn = "73"
