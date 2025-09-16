04ad6d35ce908ab89ca820de3914765987bc9434
Andrew Woods <services@awoods.net>
2025-09-16 19:07:14 -0400

    Move exported variables to bash_profile.bash
    The bash profile is the best place for exported variables.
    This makes it easier to manage them.

--- bash/bash_profile.bash
+++ bash/bash_profile.bash
@@ -2,7 +2,34 @@
 # Opal Bash Profile
 #
 
+export cals=(/usr/share/calendar/calendar.computer
+    /usr/share/calendar/calendar.history
+    /usr/share/calendar/calendar.holiday
+    /usr/share/calendar/calendar.usholiday
+    /usr/share/calendar/calendar.music
+    /usr/share/calendar/calendar.judaic
+    /usr/share/calendar/calendar.christian)
+
+EDITOR="vim"
+GIT_EDITOR=$EDITOR
+SVN_EDITOR=$EDITOR
+VISUAL=$EDITOR
+
+export EDITOR GIT_EDITOR SVN_EDITOR VISUAL
+
+# NOTE: any options specified on the command line override these values
+LESS="-"
+LESS+="g"   # highlight only the particular string which was found
+            #   by the last search command.
+LESS+="j.5" # .5 is the middle line. integer start from the top of screen
+LESS+="w"   # Temporarily highlights the first "new" line after a forward
+            #   movement of a full page.
+LESS+="x4"  # Set tab stops to 4 spaces
+PAGER="less"
+export PAGER LESS
+
 export BASH_SILENCE_DEPRECATION_WARNING=1
+export HISTTIMEFORMAT="%m-%d %a %H:%M:%S "
 
 #
 # Update the $PATH based on common directory conventions
