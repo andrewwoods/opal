6a23a208b20d8da9d35112696ff2aedfae09e923
Andrew Woods <services@awoods.net>
2026-03-25 13:59:28 -0400

    Add additional XDG directory support functions
    These functions leverage the XDG_STATE_HOME and XDG_CACHE_HOME variables

--- bash/util.bash
+++ bash/util.bash
@@ -127,6 +127,50 @@ function opal:xdg_data_dir {
 
     echo "$data_dir"
 }
+
+#
+# Return the path to the directory $XDG_STATE_HOME.
+#
+# $XDG_STATE_HOME defines the base directory relative to which user-specific
+# state files should be stored.
+#
+# @return string $directory
+#
+function opal:xdg_state_dir {
+    local data_dir
+    local xdg_default="$HOME/.local/state"
+    local create_dir
+    data_dir="${XDG_STATE_HOME:-$xdg_default}"
+
+    opal:ensure_dir_exists "$data_dir"
+
+    echo "$data_dir"
+}
+
+#
+# Return the directory path to the opal subdirectory under XDG_CACHE_HOME.
+#
+# @return string $directory
+#
+# @see opal:xdg_cache_dir
+#
+function opal:xdg_cache_dir {
+    local data_dir
+    local xdg_default="$HOME/.cache"
+    local create_dir
+    data_dir="${XDG_CACHE_HOME:-$xdg_default}"
+
+    opal:ensure_dir_exists "$data_dir"
+
+    echo "$data_dir"
+}
+
+#
+# Return the directory path to the opal subdirectory under $XDG_DATA_HOME.
+#
+# @return string $directory
+#
+# @see opal:xdg_data_dir
 #
 function opal:data_dir {
     local data_dir
@@ -140,7 +184,48 @@ function opal:data_dir {
     opal:ensure_dir_exists "$data_dir"
 
     echo "$data_dir"
+}
+
+#
+# Return the directory path to the opal subdirectory under $XDG_DATA_HOME.
+#
+# @return string $directory
+#
+# @see opal:xdg_data_dir
+#
+function opal:state_dir {
+    local data_dir
+
+    data_dir="$(opal:xdg_state_dir)"
+    if opal:is_unset "$data_dir"; then
+        return 1
     fi
+    data_dir="${data_dir}/opal"
+
+    opal:ensure_dir_exists "$data_dir"
+
+    echo "$data_dir"
+}
+
+#
+# Return the directory path to the opal subdirectory under XDG_CACHE_HOME.
+#
+# @return string $directory
+#
+# @see opal:xdg_cache_dir
+#
+function opal:cache_dir {
+    local data_dir
+
+    data_dir="$(opal:xdg_cache_dir)"
+    if opal:is_unset "$data_dir"; then
+        return 1
+    fi
+    data_dir="${data_dir}/opal"
+
+    opal:ensure_dir_exists "$data_dir"
+
+    echo "$data_dir"
 }
 
 #
