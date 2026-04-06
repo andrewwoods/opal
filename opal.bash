ef7697d56b8a0dbdf585b0a65b74edaa7ebcb358
Andrew Woods <services@awoods.net>
2026-04-05 22:20:46 -0400

    Add direcotory.bash file to opal.bash
    This should have been added when I created it.

--- opal.bash
+++ opal.bash
@@ -8,6 +8,7 @@ export OPAL_DIR="$HOME/opal"
 source $OPAL_DIR/bash/core.bash
 source $OPAL_DIR/bash/util.bash
 source $OPAL_DIR/bash/functions/developer.bash
+source $OPAL_DIR/bash/functions/directory.bash
 source $OPAL_DIR/bash/functions/system.bash
 source $OPAL_DIR/bash/functions/prompt.bash
 source $OPAL_DIR/bash/functions/string.bash
