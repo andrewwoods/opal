7ea55cf8ce934360870b91568cfa7d5da01854ce
Andrew Woods <services@awoods.net>
2026-05-14 11:53:45 -0400

    Update prompt.bash function comments
    Improve the function OpalDoc comments to better define the inputs and
    outputs for each of the prompt functions.

--- bash/functions/prompt.bash
+++ bash/functions/prompt.bash
@@ -64,6 +64,25 @@ function opal:prompt() {
     esac
 }
 
+##
+## Define the default PS1 prompt using 2 lines, for a dark theme
+##
+## This style of prompt uses two lines. Most people use a single user account on a
+## single machine. Due to this idea, the username and hostname are not included in
+## the prompt. The prompt starts with the time in 24-hour format. This is followed
+## by the base directory name. A ``~`` (tilde) will be used to represent the home
+## directory. This relies upon a dark terminal theme being used. The final piece
+## of data is the history number.
+##
+## On the second line, there is only the ``$`` (dollar symbol) which shows that
+## you're a normal user. This will be changed to a ``#`` if you're root. The nice
+## this about this style of prompt, is that it provides some detail about your
+## session, while providing a lot of space for you to write your command.
+##
+## @uses PS1
+##
+## @uses opal:color
+##
 function opal:ps1_default_dark() {
     local normal
     local bright_cyan
@@ -84,7 +103,13 @@ function opal:ps1_default_dark() {
     PS1+="${bright_white}\$${normal} "
 }
 
-
+##
+## This is the light theme companion to ``opal:ps1_default_dark``.
+##
+## @uses PS1
+##
+## @see opal:ps1_default_dark.
+##
 function opal:ps1_default_light() {
     local normal
     local cyan
@@ -106,9 +131,25 @@ function opal:ps1_default_light() {
     PS1+="${green}\$${normal} "
 }
 
-#
-#
-#
+##
+## use a typical username@host style prompt that includes the Git branch
+##
+## This style of prompt is quite common. It shows the username, name, and the
+## base directory. The home directory is displayed as a `~` (tilde). If you're
+## a Git user, the branch name is also included after the directory. There's a
+## design assumption that a dark terminal theme is being used when color is
+## passed as an argument. The default is to use text formatting like bold or
+## underline .
+##
+## @param style Optional.
+##    The value `color` is currently the only option.
+##
+## @uses opal:color
+##
+## @uses opal:parse_git_branch
+##
+## @uses PS1
+##
 function opal:ps1_brief() {
     local normal
     local blue
@@ -146,6 +187,20 @@ function opal:ps1_brief() {
     fi
 }
 
+##
+## This is the light theme companion to `opal:ps1_brief`.
+##
+## @param style Optional.
+##    The value `color` is currently the only option.
+##
+## @uses opal:color
+##
+## @uses opal:parse_git_branch
+##
+## @uses PS1
+##
+## @see opal:ps1_brief.
+##
 function opal:ps1_brief_light() {
     local style="$1"
 
@@ -179,6 +234,22 @@ function opal:ps1_brief_light() {
     fi
 }
 
+##
+## A minimal prompt that shows the current working directory with $HOME abbreviated
+## with a tilde. The history number of this command is shown for ease of command reuse.
+## The $ symbol is used to reflect you're a normal user, where the # is used for root.
+##
+## The username and hostname are not shown in this prompt because the most common
+## use case is for a person to always use their personal account on their own machine.
+## Also the git branch is not used in this prompt.
+##
+## @param style Optional.
+##    The value `color` is currently the only option.
+##
+## @uses opal:color
+##
+## @uses PS1
+##
 function opal:ps1_minimal() {
     local style="$1"
 
@@ -207,9 +278,12 @@ function opal:ps1_minimal() {
     fi
 }
 
-#
-# Secondary / Continuation Prompt
-#
+##
+## Secondary / Continuation Prompt
+##
+## This prompt is designed to connect with `opal:ps1_default_dark`, so that it
+## looks like a continuation of the PS1 command. So it's purposefully minimal.
+##
 function opal:ps2_default_dark {
    PS2=""
    PS2+="\[\e[1;37m\]"    # Color: Bright White
@@ -219,9 +293,13 @@ function opal:ps2_default_dark {
    PS2+="\[\e[0m\] "
 }
 
-#
-# Secondary / Continuation Prompt
-#
+##
+## This is the light theme companion to `opal:ps2_default_dark`.
+##
+## @uses opal:color
+##
+## @see opal:ps2_default_dark.
+##
 function opal:ps2_default_light {
 
     normal="$(opal:color normal)"
@@ -236,6 +314,11 @@ function opal:ps2_default_light {
     PS2+="${normal} "
 }
 
+##
+## The selection prompt, which is used when presenting a list of choices.
+##
+## @uses PS3
+##
 function opal:ps3_default {
     PS3=""
     PS3+="Choose a #> "
@@ -243,6 +326,11 @@ function opal:ps3_default {
     export PS3
 }
 
+##
+## The selection prompt, which is used when presenting a list of choices.
+##
+## @uses PS3
+##
 function opal:ps3_minimal {
     PS3=""
     PS3+="Select: "
@@ -250,6 +338,12 @@ function opal:ps3_minimal {
     export PS3
 }
 
+##
+## This prompt function tries to help you by vertically displaying of function
+## name file, and line number for each line of the script executed.
+##
+## @uses PS4
+##
 function opal:ps4_default {
 
     PS4="\n"
@@ -260,6 +354,10 @@ function opal:ps4_default {
     export PS4
 }
 
+##
+## This prompt function tries to help you by displaying on a single line, the
+## filename and line number for each line of the script executed.
+##
 function opal:ps4_simple {
 
 
@@ -268,6 +366,12 @@ function opal:ps4_simple {
     export PS4
 }
 
+##
+## This prompt function tries to help you by displaying on a single chevron
+## for each line of the script executed.
+##
+## @uses PS4
+##
 function opal:ps4_minimal {
     PS4='> '
 
@@ -276,8 +380,19 @@ function opal:ps4_minimal {
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-
-
+##
+## This prompt provides the most information to the user. Sometimes a git branch
+## name can be quite long. With that in mind, this prompt style displays the
+## current git branch on the top line when available. The second line displays the
+## common ``username@hostname`` pattern. The third line displays the current date
+## and time using the `opal-datetime` format, followed by the directory base name.
+##
+## @param String $style
+##
+## @uses opal:color
+##
+## @uses opal:parse_git_branch
+##
 function opal:ps1_developer {
     local normal
     local bold
@@ -315,6 +430,13 @@ function opal:ps1_developer {
     fi
 }
 
+##
+## This is the light theme companiion of opal:ps1_developer
+##
+## @uses opal:color
+##
+## @uses opal:get_date_format
+##
 function opal:ps1_developer_light {
     local normal
     local bold
